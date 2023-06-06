a2bda2b82f360742d065568ff8352552b04cc746
Anton Grinevskij <anton.grinevskij@snowflake.com>
2023-06-06 13:56:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+print("TEST")
 
 
 with st.echo(code_location='below'):
