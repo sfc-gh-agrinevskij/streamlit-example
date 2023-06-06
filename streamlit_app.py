3859a90cb50e6f991013b211dcd67872d5112757
Anton Grinevskij <anton.grinevskij@snowflake.com>
2023-06-06 13:58:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 while True:
     print("""
-    """
 # Welcome to Streamlit!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
@@ -25,8 +24,7 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-    """)
+""")
 
 
 with st.echo(code_location='below'):
